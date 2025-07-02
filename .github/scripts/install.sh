74e18649773ef423ef33c15b88103b61c822db4f
Statik DK Smoke <ascend.gremlin@gmail.com>
2025-07-02 11:41:02 -0500

    Create install.sh

---
+++ .github/scripts/install.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env zsh
+
+cd WorkFlowRepo \ &&
+ls \ &&
+cd .github/workflows \ &&
+ls \ &&
+cd .. && cd .. \ &&
+cd docs \ &&
+ls \ &&
+cd ticker-bot \ &&
+ls && cd .. \ &&
+cd graph \ &&
+ls \ &&
+cd .. && cd ..
